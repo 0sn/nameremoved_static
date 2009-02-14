62715feab708a5d52f3a8e2ac536773c7d9647ea
Nick Wolfe <github@nickwolfe.ca>
2009-02-13 22:33:05 -0500

    archive script separated out, loads properly now

---
+++ js/archive.js
@@ -0,0 +1,13 @@
+document.write('<style>#main .box .nav {display:none;}</style>');
+$(document).ready(function(){
+    $("#main .box .nav").wrap('<p class="toggler">');
+    $(".toggler").prepend("<span>Show/hide the list of comics in this storyline...</span>");
+/*  $("#main .box .nav").toggle(); */
+    $(".toggler").css("cursor","pointer");
+    
+    $("#main .box .toggler").each(function(i){
+        $(this).click(function(){
+            $(this).children("ul").slideToggle("slow");
+        });
+    });
+});
